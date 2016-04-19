21e3a1f52c232d9d9fd2c0b63dedfd114f47802d
bakterian <marcinkarczewski86@gmail.com>
2016-04-19 20:00:32 +0200

    removing untracked code from cache

--- Release/drivers/sensors/tempHumidityDHT22/TempHumiditySensor.d
+++
@@ -1,6 +0,0 @@
-drivers/sensors/tempHumidityDHT22/TempHumiditySensor.o \
-  drivers/sensors/tempHumidityDHT22/TempHumiditySensor.o:  \
- ../drivers/sensors/tempHumidityDHT22/TempHumiditySensor.cpp \
-  ../drivers/sensors/tempHumidityDHT22/TempHumiditySensor.h
-
-../drivers/sensors/tempHumidityDHT22/TempHumiditySensor.h:
--- Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := G:/3_PROGRAMOWANIE/8_C_AVR_Embeded/0_EclipseWorkspace/freeRTOS823_Static_Library/Release/libfreeRTOS823_Static_Library.a
-
-LIBS := -lm
-
